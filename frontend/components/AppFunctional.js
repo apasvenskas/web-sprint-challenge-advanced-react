2764e762a537b50df7a9a06ad38eb90386051346
Audrius Pasvenskas <apasvenskas@outlook.com>
2023-06-24 17:43:39 -0700

    project started

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // Suggested initial states
 const initialMessage = ''
@@ -9,6 +9,10 @@ const initialIndex = 4 // the index the "B" is at
 export default function AppFunctional(props) {
   // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
   // You can delete them and build your own logic from scratch.
+  const [initialMessage, setInitialMessage] = useState('');
+  const [initialEmail, setInitialEmail] = useState('');
+  const [initialSteps, setInitialSteps] = useState(0);
+  const [initialIndex, setInitialIndex] = useState(4);
 
   function getXY() {
     // It it not necessary to have a state to track the coordinates.
