80174ea83b4f91eacdeddd70ea3f651bfa524fc7
Audrius Pasvenskas <apasvenskas@outlook.com>
2023-07-04 09:45:00 -0700

    working on 5 test

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,27 @@
+import AppFunctional from './AppFunctional';
+import React from 'react';
+import { ReactDOM } from 'react';
+
+
 // Write your tests here
+
 test('sanity', () => {
   expect(true).toBe(false)
 })
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<AppFunctional />, div);
+  ReactDOM.unmountComponentAtNode(div);
+}); 
+it('renders 5 working buttons', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<AppFunctional />, div);
+  const buttons = div.querySelectorAll('keypad');
+    expect(buttons.length).toBe(5);
+    buttons.forEach(button => {
+      expect(button.disabled).toBe(false); 
+      button.click();
+    });
+    ReactDOM.unmountComponentAtNode(div);
+});
