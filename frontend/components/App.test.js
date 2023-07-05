f1f61863946fe693cbcbb76ad4d2c03eff22a21a
Audrius Pasvenskas <apasvenskas@outlook.com>
2023-07-05 09:54:51 -0700

    all complete

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,3 +1,4 @@
+import { fireEvent, render } from '@testing-library/react';
 import AppFunctional from './AppFunctional';
 import React from 'react';
 import { ReactDOM } from 'react';
@@ -24,4 +25,30 @@ it('renders 5 working buttons', () => {
       button.click();
     });
     ReactDOM.unmountComponentAtNode(div);
+});
+it('checkes for an email type input', () => {
+  const emailInput = screen.getByLabelText(/email/i);
+  expect(emailInput).toHaveAttribute('type', 'email'); 
+})
+test('reset button, resets evrything to initial state', () => {
+  const { getByText } = render(<AppFunctional />);
+  const resetButton = getByText('reset');
+  fireEvent.click(resetButton);
+  expect(getByText('initialMessage')).toBeInTheDocument();
+  expect(getByText('initialEmail')).toBeInTheDocument();
+  expect(getByText('initialSteps')).toBeInTheDocument();
+  expect(getByText('initialIndex')).toBeInTheDocument();
+  expect(getByText('2')).toBeInTheDocument();
+  expect(getByText('2')).toBeInTheDocument();
+  expect(getByText('')).toBeInTheDocumen(); 
+  expect(getByText('0')).toBeInTheDocumen();
+});
+test('renders h3 with id cordinates', () => {
+  render(<AppFunctional />);
+  const h3Coordinates = screen.getById(/coordinates/i);
+  expect(h3Coordinates).toBeInTheDocument();
+  const xValue = screen.getByText(/x:/i);
+  expect(xValue).toBeInTheDocument();
+  const yValue = screen.getByText(/y:/i);
+  expect(yValue).toBeInTheDocument();
 });
